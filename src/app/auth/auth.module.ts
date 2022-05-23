7825d84709287b6dd11d7cabb0bf26ab84410a3e
marina <marina.abdelmassih@almentor.net+1-at-486132974648>
2022-05-24 00:05:46 +0200

    init commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { RegisterComponent } from './register/register.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+
+
+@NgModule({
+  declarations: [
+    AuthComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
+})
+export class AuthModule { }
