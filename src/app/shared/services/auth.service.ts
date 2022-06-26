86e760fbc53531faa63b5a380938e85f50747214
marina <marina.abdelmassih@almentor.net+1-at-486132974648>
2022-06-26 10:44:48 +0200

    changes

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {Observable} from 'rxjs';
-import {IUserInfo} from '../interfaces/user-info.interface';
+import {IToken, IUserInfo} from '../interfaces/user-info.interface';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +12,6 @@ export class AuthService {
   }
 
   register(userData: IUserInfo):Observable<any>{
-    return this.httpClient.post(environment.baseUrl +'auth/register',{userData});
+    return this.httpClient.post(environment.baseUrl +'auth/register',userData);
   }
 }
